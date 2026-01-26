ec252aca292d4aacff908cc972539506b6295b4e
Rubal-code <rubal3232@gmail.com>
2026-01-26 12:19:37 +0530

    triangle_min_path_sum

---
+++ dynamic_programming/2 D_3D DP dp grid/triangle_min_path_sum.java
@@ -0,0 +1,15 @@
+public class triangle_min_path_sum {
+    public int minimumTotal(int[][] triangle) {
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle[i][j] + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
